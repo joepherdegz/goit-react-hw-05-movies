678da5279e77a90311685e392686e2342d17a60f
joepherdegz <142654533+joepherdegz@users.noreply.github.com>
2024-07-08 04:59:32 +0000

    Deploying to gh-pages from @ joepherdegz/goit-react-hw-05-movies@25109ab788e11900e6ad891b1c0adc62539dcb10 🚀

---
+++ static/js/520.7017efbd.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[520],{520:function(e,t,n){n.r(t);var i=n(791),r=n(689),o=n(184);t.default=function(){var e=(0,r.s0)();return(0,i.useEffect)((function(){var t=setTimeout((function(){e("/")}),2e3);return function(){return clearTimeout(t)}}),[e]),(0,o.jsx)("div",{style:{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",textAlign:"center",color:"orange"},children:"Not Found, Redirecting to homepage..."})}}}]);
+//# sourceMappingURL=520.7017efbd.chunk.js.map
